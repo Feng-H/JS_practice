9e6e5c87a4b62eda5539e14ac27406e120cf90dd
Feng-H <53200542+Feng-H@users.noreply.github.com>
2021-02-28 22:18:11 +0800

    0.1

---
+++ hidden sth do not want.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hidden sth do not want
+// @namespace    https://feng-h.github.io/
+// @version      0.1
+// @description  first Tampermonkey Script and practice JavaScript
+// @author       Feng-H
+// @match        https://www.baidu.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const msg = 'Hello from Feng-H on BAIDU';
+    alert(msg);
+})();
---
+++ my.js
@@ -0,0 +1,3 @@
+const addNums = (num1 = 1，num2 =1) => console.log(num1 + num2);
+
+addNums(4,9);
